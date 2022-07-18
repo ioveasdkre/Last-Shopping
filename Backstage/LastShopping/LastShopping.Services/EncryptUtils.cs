6bd7ff8c78e90cc8ec2a2458c3a43784dfbda3d4
ERI\ioveasdkre <a96080103>
2022-07-18 18:57:52 +0800

    新增 SHA256加密

---
+++ Backstage/LastShopping/LastShopping.Services/EncryptUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LastShopping.Services
+{
+    public class EncryptUtils
+    {
+        #region SHA256Encrypt [ SHA256加密 ]
+        /// <summary>SHA256加密</summary>
+        /// <param name="text">本文</param>
+        /// <returns></returns>
+        public static string SHA256Encrypt(string text)
+        {
+            // 判斷 字串為 null or ""
+            if (string.IsNullOrEmpty(text))
+                return string.Empty; // 空字串
+
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            byte[] hash = SHA256.Create().ComputeHash(bytes);
+
+            return Convert.ToBase64String(hash);
+        }
+        #endregion
+    }
+}
